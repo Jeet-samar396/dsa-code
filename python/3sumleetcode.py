507604dc8c39d38a9992abf9af2857e94da90e5a
jeet-samar <mailsamar03@gmail.com>
2025-10-05 19:48:31 +0530

    Added Python DSA Code

--- python/3sumleetcode.py
+++ python/3sumleetcode.py
@@ -1,33 +1,10 @@
-from typing import List
+# Function to sum three numbers
+def sum_of_three(a, b, c):
+    return a + b + c
 
-class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        nums.sort()
-
-        for i in range(len(nums)):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue  
-
-            j, k = i + 1, len(nums) - 1
-
-            while j < k:
-                total = nums[i] + nums[j] + nums[k]
-
-                if total > 0:
-                    k -= 1
-                elif total < 0:
-                    j += 1
-                else:
-                    res.append([nums[i], nums[j], nums[k]])
-                    j += 1
-
-                    
-                    while j < k and nums[j] == nums[j - 1]:
-                        j += 1
-        return res
-    
-    
-nums = [-1, 0, 1, 2, -1, -4]
-solution = Solution()
-print(solution.threeSum(nums))
+# Example usage
+num1 = 10
+num2 = 20
+num3 = 30
+result = sum_of_three(num1, num2, num3)
+print("Sum of three numbers is:", result)
